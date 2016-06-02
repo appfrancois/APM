6feab006943ebaa4a2d8429a4689eb4095a79b43
appfrancois <franswa@Francois-Duchemins-iMac-2.local>
2016-06-02 21:55:58 +0200

    Add star shared component

--- app/products/product-list.component.ts
+++ app/products/product-list.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from 'angular2/core';
 import { IProduct } from './product';
 import { ProductFilterPipe } from './product-filter.pipe';
+import { StarComponent } from '../shared/star.component';
 
 @Component({
     selector: 'pm-products',
     templateUrl: 'app/products/product-list.component.html', // relative to index.html!
     styleUrls: ['app/products/product-list.component.css'],
-    pipes: [ProductFilterPipe]
+    pipes: [ProductFilterPipe],
+    directives: [StarComponent]
 })
 export class ProductListComponent implements OnInit {
     pageTitle: string = 'Product List';
@@ -53,5 +55,9 @@ export class ProductListComponent implements OnInit {
     
     ngOnInit(): void {
         console.log('In on init');
+    }
+    
+    onRatingClicked(message: string): void {
+        this.pageTitle = 'Product List: ' + message;
     }
 }
